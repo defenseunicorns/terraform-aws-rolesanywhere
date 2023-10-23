32a6090c35f11cddbb38af611d9c152f20bbd735
Zack Annexstein <zannexstein@gmail.com>
2023-10-23 07:40:50 -0700

    remove

--- examples/complete/output.tf
+++ examples/complete/output.tf
@@ -1,7 +1,3 @@
 output "certificate_files" {
   value = keys(data.tls_certificate.dod_cert)
 }
-
-output "certificate_pems" {
-  value = [for cert in data.tls_certificate.dod_cert : cert.certificates[*].cert_pem]
-}
